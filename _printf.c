c42fdfab1bd36caf7a2695a18a93f126f5b76813
Gnl2005 <allahverdiyevasofiya@gmail.com>
2025-08-29 16:06:54 +0000

    K

--- _printf.c
+++ _printf.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * _printf - a function that produces output according to a format
@@ -9,12 +10,59 @@
  */
 int _printf(const char *format, ...)
 {
-	char g[] = "GNL";
+    va_list args;
+    int count = 0;
+    char *str;
+    char c;
 
-	g[0] = 'G';
+    if (!format)
+        return (0); /* NULL olsa 0 qaytarır */
 
-	write(1, "Name: GNL\n", 10);
-	write(1, "Char: G\n", 8);
-	write(1, "%\n", 2);
-	return (0);
+    va_start(args, format);
+
+    /* for dövrü ilə hər simvolu oxuyuruq */
+    for (; *format != '\0'; format++)
+    {
+        if (*format == '%')
+        {
+            format++; /* növbəti simvol format tipidir */
+
+            if (*format == 'c') /* char */
+            {
+                c = (char) va_arg(args, int);
+                write(1, &c, 1);
+                count++;
+            }
+            else if (*format == 's') /* string */
+            {
+                str = va_arg(args, char *);
+                if (!str)
+                    str = "(null)";
+                for (; *str != '\0'; str++)
+                {
+                    write(1, str, 1);
+                    count++;
+                }
+            }
+            else if (*format == '%') /* procent */
+            {
+                write(1, "%", 1);
+                count++;
+            }
+            else /* unknown format */
+            {
+                write(1, "%", 1);
+                write(1, format, 1);
+                count += 2;
+            }
+        }
+        else /* normal simvol */
+        {
+            write(1, format, 1);
+            count++;
+        }
+    }
+
+    va_end(args);
+    return (count);
 }
