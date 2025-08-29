25ea2003c4e813270a79d67fc2dd413dfc7aa4ca
Gnl2005 <allahverdiyevasofiya@gmail.com>
2025-08-29 16:53:48 +0000

    K

--- _printf.c
+++ _printf.c
@@ -1,80 +1,127 @@
-#include "main.h"
-#include <unistd.h>
+#include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
 
 /**
- * print_char - prints a single char
- * @c: character to print
- *
- * Return: 1
+ * print_char - print a character to stdout
+ * @args: list of argumetns from printf
+ * @length: length of what is to be printed
+ * Return: length
  */
-int print_char(char c)
+
+int print_char(va_list args, int length)
 {
-	write(1, &c, 1);
-	return (1);
+	putchar(va_arg(args, int));
+	return (length + 1);
 }
 
 /**
- * print_string - prints a string
- * @str: string to print
- *
- * Return: number of characters printed
+ * print_string - print a string to stdout
+ * @args: list of argumetns from printf
+ * @length: length of what is going to be printed
+ * Return: length
  */
-int print_string(char *str)
+
+int print_string(va_list args, int length)
 {
-	int count = 0;
+	int i;
+	char nil[7] = "(null)", *str;
 
-	if (!str)
-		str = "(null)";
-
-	while (*str)
+	str = va_arg(args, char *);
+	if (str != NULL)
 	{
-		write(1, str, 1);
-		str++;
-		count++;
+		for (i = 0; str[i]; i++)
+			putchar(str[i]);
+		length += i;
+		return (length);
 	}
-	return (count);
+	for (i = 0; nil[i]; i++)
+		putchar(nil[i]);
+	length += 6;
+	return (length);
 }
 
 /**
- * _printf - prints formatted output
- * @format: format string
- *
- * Return: number of characters printed
+ * handle_mod - handle modulo sign
+ * @args: list of argumetns from printf
+ * @length: length of what is to be printed
+ * Return: length
  */
+
+int handle_mod(va_list args, int length)
+{
+	(void) args;
+
+	putchar('%');
+	return (length + 1);
+}
+
+/**
+ * print_int - handles printing numbers
+ * @args: list of argumetns from printf
+ * @length: length of what is to be printed ouit to stdout
+ * Return: length
+ */
+
+int print_int(va_list args, int length)
+{
+	int i;
+	char number[100];
+
+	sprintf(number, "%d", va_arg(args, int));
+	for (i = 0; number[i]; i++)
+		putchar(number[i]);
+	length += i;
+	return (length);
+}
+
+/**
+ * _printf - print argument according to format specifier
+ * @format: what should be printed
+ * Return: length
+ */
+
 int _printf(const char *format, ...)
 {
+	int i, j, length = 0;
 	va_list args;
-	int count = 0;
-
-	if (!format)
-		return (0);
+	spec format_spec[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", handle_mod},
+		{"i", print_int},
+		{"d", print_int},
+		{NULL, NULL}
+	};
 
 	va_start(args, format);
-
-	for (; *format != '\0'; format++)
+	if (format == NULL)
+		return (-1);
+	for (i = 0; format[i]; i++)
 	{
-		if (*format == '%')
+		if (format[i] == '%')
 		{
-			format++;
-			if (*format == '\0')
-				break;
-			else if (*format == 'c')
-				count += print_char((char)va_arg(args, int));
-			else if (*format == 's')
-				count += print_string(va_arg(args, char *));
-			else if (*format == '%')
-				count += print_char('%');
-			else
+			if (strlen(format) < 2 && format[i] == '%')
+				return (-1);
+			for (j = 0; format_spec[j].specifier != NULL; j++)
 			{
-				count += print_char('%');
-				count += print_char(*format);
+				if (format[i + 1] == format_spec[j].specifier[0])
+				{
+					length = format_spec[j].func(args, length);
+					i++;
+					break;
+				}
 			}
+		} else
+		{
+			if (i == 1 && strlen(format) <= 3)
+				return (write(1, format, strlen(format)));
+			putchar(format[i]);
+			length++;
 		}
-		else
-			count += print_char(*format);
 	}
-
-	va_end(args);
-	return (count);
+	return (length);
 }
